42665fce38fc78856f70e8fc6e0116f6be540808
Serhat S <serhats@tutamail.com>
2020-12-21 22:09:05 +0300

    Implement LinkedList

--- packages/data-structures/src/List/LinkedList.ts
+++ packages/data-structures/src/List/LinkedList.ts
@@ -1,27 +1,101 @@
 import IList from './IList';
 
-export default class LinkedList<T> implements IList<T> {
-  length: number;
-  
+class Node<T extends Object> {
+  data: T;
+  next: Node<T>;
+
+  constructor(data: T, next: Node<T> = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+export default class LinkedList<T extends Object> implements IList<T> {
+  length: number = 0;
+
+  private head: Node<T> = null;
+
   push(data: T): void {
-    throw new Error('Method not implemented.');
+    if (this.length == 0) {
+      this.head = new Node(data);
+    } else {
+      let iter = this.head;
+      while (iter.next != null) iter = iter.next;
+      iter.next = new Node(data);
+    }
+    this.length++;
   }
+
   pushFront(data: T): void {
-    throw new Error('Method not implemented.');
+    let newHead = new Node(data);
+    newHead.next = this.head;
+    this.head = newHead;
+    this.length++;
   }
+
   pop(): T {
-    throw new Error('Method not implemented.');
+    if (this.length == 0) throw new Error('There is no element to pop');
+    if (this.length == 1) {
+      let data = this.head.data;
+      this.head = null;
+      this.length = 0;
+      return data;
+    }
+
+    let prev: Node<T>;
+    let iter = this.head;
+    while (iter.next != null) {
+      prev = iter;
+      iter = iter.next;
+    }
+    let data = iter.data;
+    prev.next = null;
+    this.length--;
+    return data;
   }
+
   popFront(): T {
-    throw new Error('Method not implemented.');
+    if (this.length == 0) throw new Error('There is no element to pop');
+
+    let data = this.head.data;
+    this.head = this.head.next;
+    this.length--;
+    return data;
   }
+
   get(index: number): T {
-    throw new Error('Method not implemented.');
+    if (index < 0 || index >= this.length)
+      throw new Error('Index out of list range!');
+
+    let iter = this.head;
+    for (let i = 0; i < index; i++) iter = iter.next;
+    return iter.data;
   }
+
   removeAt(index: number): T {
-    throw new Error('Method not implemented.');
+    if (index < 0 || index >= this.length)
+      throw new Error('Index out of list range!');
+
+    let prev: Node<T> = this.head;
+    let iter = this.head;
+    for (let i = 0; i < index; i++) {
+      prev = iter;
+      iter = iter.next;
+    }
+    let data = iter.data;
+    prev.next = iter.next;
+    this.length--;
+    return data;
   }
+
   toString(): string {
-    throw new Error('Method not implemented.');
+    let print = '[ ';
+    let iter = this.head;
+    while (iter != null) {
+      print += `${iter.data.toString()} `;
+      iter = iter.next;
+    }
+    print += ']';
+    return print;
   }
 }
