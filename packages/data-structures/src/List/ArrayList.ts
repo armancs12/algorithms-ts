b41026adad80cc55b5f013a00f3c8c3cc890ad1c
Serhat S <serhats@tutamail.com>
2020-12-24 15:02:01 +0300

    Implement ArrayList

--- packages/data-structures/src/List/ArrayList.ts
+++ packages/data-structures/src/List/ArrayList.ts
@@ -2,32 +2,79 @@ import IList from './IList';
 
 export default class ArrayList<T> implements IList<T> {
   length: number;
+  private array: Array<T>;
+  private arrayLength: number;
 
-  push(_data: T): void {
-    throw new Error('Method not implemented.');
+  constructor() {
+    this.length = 0;
+    this.array = new Array<T>(1);
+    this.arrayLength = 1;
   }
 
-  pushFront(_data: T): void {
-    throw new Error('Method not implemented.');
+  private increaseArray() {
+    this.arrayLength = this.arrayLength * 2;
+    const newArray = new Array(this.arrayLength);
+    for (let i = 0; i < this.length; i++) newArray[i] = this.array[i];
+    this.array = newArray;
+  }
+
+  private decreaseArray() {
+    this.arrayLength = this.arrayLength / 2;
+    const newArray = new Array(this.arrayLength);
+    for (let i = 0; i < this.length; i++) newArray[i] = this.array[i];
+    this.array = newArray;
+  }
+
+  push(data: T): void {
+    if (this.length == this.arrayLength) this.increaseArray();
+    this.array[this.length++] = data;
+  }
+
+  pushFront(data: T): void {
+    if (this.length == this.arrayLength) this.increaseArray();
+    for (let i = this.length; i > 0; i--) this.array[i] = this.array[i - 1];
+    this.array[0] = data;
+    this.length++;
   }
 
   pop(): T {
-    throw new Error('Method not implemented.');
+    if (this.length == 0) throw new Error('There is no element to pop');
+    const data = this.array[this.length - 1];
+    this.array[--this.length] = undefined;
+    if (this.arrayLength > 1 && this.length < this.arrayLength / 2)
+      this.decreaseArray();
+    return data;
   }
 
   popFront(): T {
-    throw new Error('Method not implemented.');
+    if (this.length == 0) throw new Error('There is no element to pop');
+    const data = this.array[0];
+    for (let i = 0; i < this.length; i++) this.array[i] = this.array[i + 1];
+    this.array[--this.length] = undefined;
+    if (this.arrayLength > 1 && this.length < this.arrayLength / 2)
+      this.decreaseArray();
+    return data;
   }
 
-  get(_index: number): T {
-    throw new Error('Method not implemented.');
+  get(index: number): T {
+    if (index < 0 || index >= this.length)
+      throw new Error('Index out of list range!');
+    return this.array[index];
   }
 
-  removeAt(_index: number): T {
-    throw new Error('Method not implemented.');
+  removeAt(index: number): T {
+    if (index < 0 || index >= this.length)
+      throw new Error('Index out of list range!');
+    const data = this.array[index];
+    for (let i = index; i < this.length; i++) this.array[i] = this.array[i + 1];
+    this.array[--this.length] = undefined;
+    return data;
   }
 
   toString(): string {
-    throw new Error('Method not implemented.');
+    let print = '[ ';
+    for (const data of this.array)
+      if (data != undefined) print += `${data.toString()} `;
+    return print + ']';
   }
 }
