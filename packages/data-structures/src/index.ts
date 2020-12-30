486e0771b784b2b77a7ef7a6aa3b5a5cd7b45e92
Serhat S <serhats@tutamail.com>
2020-12-30 22:49:02 +0300

    Export interfaces

--- packages/data-structures/src/index.ts
+++ packages/data-structures/src/index.ts
@@ -1,7 +1,10 @@
+export { default as IList } from './List/IList';
 export { default as LinkedList } from './List/LinkedList';
 export { default as ArrayList } from './List/ArrayList';
+export { default as IStack } from './Stack/IStack';
 export { default as LinkedStack } from './Stack/LinkedStack';
 export { default as ArrayStack } from './Stack/ArrayStack';
+export { default as IQueue } from './Queue/IQueue';
 export { default as LinkedQueue } from './Queue/LinkedQueue';
 export { default as ArrayQueue } from './Queue/ArrayQueue';
 export { default as ArrayPriorityQueue } from './Queue/ArrayPriorityQueue';
